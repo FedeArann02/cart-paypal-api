e0e08c11f488cca351bdcb6ed75e07da1ee62dbc
Fede Arán <aranfederico3@gmail.com>
2026-08-06 19:14:52 -0300

    fixeando errores de tipeo

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const pool = require("../config/db")
 
 const showRoleByUserId = async (idAuthSupabase) => {
-    const { rows } = await pool.query("SELET role FROM users WHERE id_auth_supabase = $1", [idAuthSupabase])
+    const { rows } = await pool.query("SELECT role FROM users WHERE id_auth_supabase = $1", [idAuthSupabase])
     return rows[0]?.role
 }
 
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController.js");
 const authenticate = require("../middleware/authenticate.js")
+const isAdmin = require("../middleware/isAdmin.js")
 
 router.get("/", productController.getAllProducts)
 router.get("/:id", productController.getProductById)
 
-router.post("/", authenticate, productController.createProduct)
-router.put("/:id", authenticate, productController.updateProduct)
-router.delete("/:id", authenticate, productController.deleteProduct)
+router.post("/", authenticate, isAdmin, productController.createProduct)
+router.put("/:id", authenticate, isAdmin, productController.updateProduct)
+router.delete("/:id", authenticate, isAdmin, productController.deleteProduct)
 
 module.exports = router;
