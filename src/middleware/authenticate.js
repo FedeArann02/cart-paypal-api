a20a0b3d32ae4c387011262b46f67fd39ddbd576
Fede Arán <aranfederico3@gmail.com>
2026-08-06 18:05:24 -0300

    Middleware authenticate para los users

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,5 +1,16 @@
 const supabase = require("../config/supabase")
 
-const authenticate = async(req, res, next) =>{
-    const token = req.headers.autorizarion
-}
+const authenticate = async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1] //separamos el value del auth Bearer-token
+    if (!token) {
+        return res.status(401).json({ error: "Unauthorized" })
+    }
+    const { data: { user }, error } = await supabase.auth.getUser(token)
+    if (error || !user) {
+        return res.status(401).json({ error: "Unauthorized" })
+    }
+    req.user = user
+    next()
+};
+
+module.exports = authenticate
