88a24237c970effbcdcba8d41177e0a159c28990
Fede Arán <aranfederico3@gmail.com>
2026-08-10 15:03:57 -0300

    paypalController (create payment, execute payment) terminado

--- src/config/paypal.js
+++ src/config/paypal.js
@@ -1,1 +1,10 @@
 //npm i paypal-rest-sdk
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+  'mode': process.env.PAYPAL_MODE,
+  'client_id': process.env.PAYPAL_CLIENT_ID,
+  'client_secret': process.env.PAYPAL_CLIENT_SECRET
+});
+
+module.exports = paypal
---
+++ src/controllers/paypalController.js
@@ -0,0 +1,68 @@
+const paypal = require("../config/paypal")
+const { updateSaleState } = require("../models/SaleModel")
+
+const createPayment = async (req, res, next) => {
+    const create_payment_json = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal"
+        },
+        redirect_urls: {
+            return_url: "http://return.url",
+            cancel_url: "http://cancel.url"
+        },
+        transactions: [{
+            item_list: {
+                items: req.body.items
+            },
+            amount: {
+                currency: "USD",
+                total: req.body.total
+            },
+            description: "Esta es la estructura de pagos con paypal"
+        }]
+    };
+
+    paypal.payment.create(create_payment_json, async function (error, payment) {
+        if (error) {
+            next(error)
+        }
+        else {
+            res.status(200).json({ payment })
+        }
+    })
+};
+
+const executePayment = async (req, res, next) => {
+    const { sale_id } = req.params
+    const { paymentId, payerId } = req.query
+    const execute_payment_json = {
+        payer_id: payerId,
+        transactions: [{
+            amount: {
+                currency: "USD",
+                total: req.query.total
+            }
+        }]
+    }
+    paypal.payment.execute(paymentId, execute_payment_json, async function (error, payment) {
+        if (error) {
+            next(error)
+        }
+        else {
+            try {
+                await updateSaleState("CONFIRMADO", sale_id)
+                console.log("completo")
+                res.status(200).json({ payment })
+
+            } catch (error) {
+                next(error)
+            }
+        }
+    })
+}
+
+module.exports = {
+    createPayment,
+    executePayment
+}
